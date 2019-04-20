32d9da740117d76cc413f61a87b8140871847c49
user <muatdip127@yandex.ru>
2019-04-20 22:16:44 +0300

    First Commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-date-picker
+      v-model="date"
+      width="290"
+      class="mt-3"
+    ></v-date-picker>
+    <v-date-picker
+      v-model="date"
+      full-width
+      landscape
+      class="mt-3"
+    ></v-date-picker>
+  </div>
+</template>
+
+<script>
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import "vue-material-design-icons/styles.css"
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(Vuetify);
+  export default {
+    data: () => ({
+      date: new Date().toISOString().substr(0, 10)
+    })
+  }
+</script>
